e6526d117dc4f63dbf805af528e51500fc4c82f6
Julian Yap <julianokyap@gmail.com>
2026-03-23 16:07:52 -0700

    Add Vial keyboard firmware for Pando58

---
+++ keyboards/jyap808/pando58/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD8, 0x1F, 0x9A, 0x85, 0x23, 0xE5, 0xEA, 0x07}
+
+// Vial unlock
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Vial features
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
